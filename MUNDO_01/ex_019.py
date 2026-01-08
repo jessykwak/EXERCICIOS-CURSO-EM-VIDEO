ea79e52a95505e862d8dcd927042fb1fa6445b83
Jessica Kwak <jessica92kr@gmail.com>
2026-01-07 23:12:35 -0300

    desenvolvido o ex 19 e 20

--- MUNDO_01/ex_019.py
+++ MUNDO_01/ex_019.py
@@ -2,15 +2,29 @@
 # Um professor quer sortear um dos seus quatro alunos para apagar o quadro. 
 # Faca um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
 
-import random
+from random import choice
 
+#resolucao com lista
 nomes = []
 
-nomes.append = input('Digite o nome do aluno: ')
-nomes.append = input('Digite o nome do aluno: ')
-nomes.append = input('Digite o nome do aluno: ')
-nomes.append = input('Digite o nome do aluno: ')
+nomes.append(input('Digite o nome do aluno: '))
+# OBS: .append faz uma acao. entao nomes.append = input... nao faz sentido
+# pq esta substituindo nomes.append(uma acao) com um input.
+nomes.append(input('Digite o nome do aluno: '))
+nomes.append(input('Digite o nome do aluno: '))
+nomes.append(input('Digite o nome do aluno: '))
 
-e = random.choice(nomes)
+e = choice(nomes)
 
 print('O aluno selecionado foi: {}'.format(e))
+
+#resolucao alternativa
+
+a = input('Digite o nome do aluno: ')
+b = input('Digite o nome do aluno: ')
+c = input('Digite o nome do aluno: ')
+d = input('Digite o nome do aluno: ')
+
+e1 = choice((a, b, c, d))
+
+print('O aluno selecionado foi: {}'.format(e1))
--- MUNDO_01/ex_020.py
+++ MUNDO_01/ex_020.py
@@ -1,3 +1,16 @@
 # EXERCICIO 020:
 # O mesmo professor do desafio anterior quer sortear a ordem de apresentacao de trabalhos dos alunos. 
 # Faca um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
+
+from random import shuffle
+
+nomes = []
+
+nomes.append(input('Digite o nome do aluno: '))
+nomes.append(input('Digite o nome do aluno: '))
+nomes.append(input('Digite o nome do aluno: '))
+nomes.append(input('Digite o nome do aluno: '))
+
+e = shuffle(nomes)
+
+print('A ordem de apresentacao sera: {}'.format(nomes))

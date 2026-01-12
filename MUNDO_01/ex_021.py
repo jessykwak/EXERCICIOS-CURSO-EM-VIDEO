de96ec39ad69d0cf2ae62e112ca01674253390bd
Jessica Kwak <jessica92kr@gmail.com>
2026-01-11 23:50:17 -0300

    ex 21 atualizado

--- MUNDO_01/ex_021.py
+++ MUNDO_01/ex_021.py
@@ -1,2 +1,12 @@
 # EXERCICIO 021:
 # Faca um programa em python que abra e reproduza o audio de um arquivo mp3.
+
+import pygame
+
+musica = input('Digite o caminho do arquivo de musica: ')
+pygame.mixer.init()  
+pygame.mixer.music.load(musica)
+pygame.mixer.music.play()
+
+while pygame.mixer.music.get_busy() == True:
+    continue

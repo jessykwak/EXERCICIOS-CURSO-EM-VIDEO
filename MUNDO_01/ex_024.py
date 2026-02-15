ff3a52e10bb3db9282cdc721be2f44434ffd0f31
Jessica Kwak <jessica92kr@gmail.com>
2026-02-15 20:09:13 -0300

    mend

--- MUNDO_01/ex_024.py
+++ MUNDO_01/ex_024.py
@@ -1,2 +1,11 @@
 # EXERCICIO 024:
 # Crie um programa que leia o nome de uma cidade e diga se ela ccomeça ou nao com o nome 'SANTO'.
+
+cidade = str(input('Digite o nome da cidade:'))
+primeiro = cidade.split()
+
+
+if (primeiro[0].upper() == 'SANTO'):
+    print('COMEÇA COM SANTO!')
+else:
+    print('NAO COMEÇA COM SANTO')
